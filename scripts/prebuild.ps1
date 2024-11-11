38f913477975f36ca28cbe8b8b6d41166fed2a03
yutai <yutai.ke@bse.eu>
2024-11-11 19:00:10 +0100

    Fix dd

--- scripts/prebuild.ps1
+++ scripts/prebuild.ps1
@@ -1,10 +1,8 @@
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
 echo "Now set up vcpkg"
 git clone https://github.com/microsoft/vcpkg.git
+$env:VCPKG_ROOT = "D:\a\vrp_solver_ignore\vrp_solver_ignore\vcpkg"
 .\vcpkg\bootstrap-vcpkg.bat
 .\vcpkg\vcpkg.exe install boost-python boost-graph
-echo Current directory before setting VCPKG_ROOT: %CD%
-set "VCPKG_ROOT=%CD%\vcpkg"
-echo VCPKG_ROOT set to %VCPKG_ROOT%
 echo "Now build cpp proj"
 .\scripts\build.bat
