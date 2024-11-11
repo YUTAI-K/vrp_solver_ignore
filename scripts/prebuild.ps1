3bd1ae991ff92a3ebcf970ea25bcd085dee7cf30
yutai <yutai.ke@bse.eu>
2024-11-12 00:18:59 +0100

    Try static linking

--- scripts/prebuild.ps1
+++ scripts/prebuild.ps1
@@ -3,6 +3,13 @@ echo "Now set up vcpkg"
 git clone https://github.com/microsoft/vcpkg.git
 $env:VCPKG_ROOT = "D:\a\vrp_solver_ignore\vrp_solver_ignore\vcpkg"
 .\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg.exe install boost-python boost-graph
+if ([Environment]::Is64BitOperatingSystem) {
+    $triplet = "x64-windows-static"
+    Write-Host "Detected 64-bit OS. Using triplet: $triplet"
+} else {
+    $triplet = "x86-windows-static"
+    Write-Host "Detected 32-bit OS. Using triplet: $triplet"
+}
+.\vcpkg\vcpkg.exe install boost-python boost-graph --triplet $triplet
 echo "Now build cpp proj"
 .\scripts\build.bat
